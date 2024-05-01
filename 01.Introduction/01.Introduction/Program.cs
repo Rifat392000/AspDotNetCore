1737c275f278b11cdd62525f01f85d1417e5bf81
Rifat392000 <arrifat392000@outlook.com>
2024-05-01 12:14:52 +0600

    Asp Dot Net Core [Kestrel, IIS Express]

---
+++ 01.Introduction/01.Introduction/Program.cs
@@ -0,0 +1,15 @@
+namespace _01.Introduction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Welcome To All");
+
+            app.Run();
+        }
+    }
+}
